69c8c88010dc774540d8645f63010614baffc731
balkangraph <zornitsa.pesheva@outlook.com>
2023-09-09 10:03:24 +0300

    first commit

---
+++ src/mytree.tsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import OrgChart from '@balkangraph/orgchart.js';
+type MyProps = OrgChart.options;
+export default class extends Component<MyProps> {
+    private divRef: React.RefObject<HTMLInputElement>;
+    chart: OrgChart | undefined;
+
+    constructor(props: {}) {
+        super(props);
+        this.divRef = React.createRef();
+    }
+
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    componentDidMount() {
+        if (this.divRef.current != null)
+            this.chart = new OrgChart(this.divRef.current, {
+                nodes: this.props.nodes,
+
+                nodeBinding: {
+                    field_0: "name",
+                    img_0: "img"
+                }
+            });
+    }
+
+    render() {
+        return (
+            <div id="tree" ref={this.divRef}></div>
+        );
+    }
+}
