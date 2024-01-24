26d8407ff534ef45344034b48dcf179f734719f1
balkangraph <zornitsa.pesheva@outlook.com>
2024-01-24 08:01:05 +0200

    create your own template

--- src/mytree.tsx
+++ src/mytree.tsx
@@ -15,15 +15,22 @@ export default class Chart extends Component<MyProps> {
     }
 
     componentDidMount() {
-        if (this.divRef.current != null)
+        if (this.divRef.current != null) {
+            // how to create your own template
+            OrgChart.templates.myTemplate = Object.assign({}, OrgChart.templates.ana);
+            OrgChart.templates.myTemplate.node =
+                '<rect filter="url(#isla-shadow1)" x="0" y="0" rx="5" ry="5" height="120" width="250" fill="#FFF" stroke-width="1" stroke="#1EC9E8" ></rect> ';
+
             this.chart = new OrgChart(this.divRef.current, {
                 nodes: this.props.nodes,
-
+                template: "myTemplate",
                 nodeBinding: {
                     field_0: "name",
                     img_0: "img"
                 }
             });
+        }
+        
     }
 
     render() {
